7954fc5576b10bfc6dabb3047ce9a256ecfdf698
YourBoyE <egcrossmover@gmail.com>
2019-11-19 15:24:41 -0800

    All visuals are in place and now I need to begin coding

---
+++ RemindMe/DisplayVC.swift
@@ -0,0 +1,30 @@
+//
+//  DisplayVC.swift
+//  RemindMe
+//
+//  Created by YBE on 11/19/19.
+//  Copyright © 2019 DreamDev. All rights reserved.
+//
+
+import UIKit
+
+class DisplayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RemindMe/ToDoTVC.swift
@@ -0,0 +1,37 @@
+//
+//  ToDoTVC.swift
+//  RemindMe
+//
+//  Created by YBE on 11/19/19.
+//  Copyright © 2019 DreamDev. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+   
+
+}
